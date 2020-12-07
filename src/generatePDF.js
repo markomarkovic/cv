b8a94067e2a7a000e13af3ac4a180105a8128ecc
Marko Marković <okram@civokram.com>
2020-12-07 23:37:03 +0400

    Add PDF generation

---
+++ src/generatePDF.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const puppeteer = require('puppeteer')
+
+async function generatePDF() {
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.goto('http://localhost:8080/', { waitUntil: 'networkidle0' })
+  const pdf = await page.pdf({
+    format: 'A4',
+    printBackground: true,
+    displayHeaderFooter: false,
+  })
+  await browser.close()
+
+  const saveFile = path.resolve(
+    __dirname,
+    '..',
+    'dist',
+    'Marko Markovic - CV - EN.pdf'
+  )
+  fs.writeFileSync(saveFile, pdf)
+}
+
+generatePDF()
