44de17e2b2f7498be6dea31d99840cd11bee8f55
Marko Marković <okram@civokram.com>
2020-12-08 23:55:39 +0400

    Tweak print styles some more

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        print: { raw: 'print' },
+      },
+    },
   },
   variants: {
     extend: {},
