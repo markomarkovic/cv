b5a9bf255ed2d78a67a2a751852bea46e87b2c0a
Marko Marković <okram@civokram.com>
2020-12-09 10:20:57 +0400

    Fix linting

--- src/facts/quickFacts.js
+++ src/facts/quickFacts.js
@@ -1,24 +1,26 @@
 export default [
   {
-    icon: import.meta.env.BASE_URL + 'img/icons/placemark.svg',
-    text: 'Currently in <b>Abu Dhabi, United Arab Emirates</b> for work (2 years and counting), with a home base in Belgrade, Serbia.',
+    icon: `${import.meta.env.BASE_URL}img/icons/placemark.svg`,
+    text:
+      'Currently in <b>Abu Dhabi, United Arab Emirates</b> for work (2 years and counting), with a home base in Belgrade, Serbia.',
   },
   {
-    icon: import.meta.env.BASE_URL + 'img/icons/hourglass.svg',
+    icon: `${import.meta.env.BASE_URL}img/icons/hourglass.svg`,
     text:
       'Born in 1980 &mdash; Experienced analog childhood and a digital adulthood. Posesses both Generation-X cynicism and Millenial optimism and drive.',
   },
   {
-    icon: import.meta.env.BASE_URL + 'img/icons/i18n.svg',
-    text: 'Understands, speaks and writes in <b>Serbian</b> <small>(native)</small>, <b>English</b> <small>(full professional proficiency)</small>, and <b>German</b> <small>(professional working proficiency)</small>.',
+    icon: `${import.meta.env.BASE_URL}img/icons/i18n.svg`,
+    text:
+      'Understands, speaks and writes in <b>Serbian</b> <small>(native)</small>, <b>English</b> <small>(full professional proficiency)</small>, and <b>German</b> <small>(professional working proficiency)</small>.',
   },
   {
-    icon: import.meta.env.BASE_URL + 'img/icons/envelope.svg',
+    icon: `${import.meta.env.BASE_URL}img/icons/envelope.svg`,
     text:
       'Prefers async communication: <a href="mailto:okram@civokram.com">okram@civokram.com</a>',
   },
   {
-    icon: import.meta.env.BASE_URL + 'img/icons/phone.svg',
+    icon: `${import.meta.env.BASE_URL}img/icons/phone.svg`,
     text: 'Sometimes uses the phone: <b>+381 62 200 512</b>',
   },
 ]
