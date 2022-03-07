618adca9b54d91601d6461c09d88335833acc215
Marko Marković <okram@civokram.com>
2022-03-07 13:54:59 +0400

    WIP: root files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [Marko Markovic — Curriculum Vitae](https://markomarkovic.github.io/cv/)
 
-> Personal CV, now built with Vue and Tailwind CSS, with automatic generation of a [PDF version](https://markomarkovic.github.io/cv/Marko%20Markovic%20-%20CV%20-%20EN.pdf) using Puppeteer, and automatically published to GitHub Pages.
+> Personal CV, now built with Vue and UnoCSS, with automatic generation of a [PDF version](https://markomarkovic.github.io/cv/Marko%20Markovic%20-%20CV%20-%20EN.pdf) using Puppeteer, and automatically published to GitHub Pages.
 
 ## License
 
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,40 @@
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { presetIcons, presetUno } from 'unocss'
+import Unocss from 'unocss/vite'
+import { defineConfig } from 'vite'
 
-/**
- * https://vitejs.dev/config/
- * @type {import('vite').UserConfig}
- */
-export default {
-  plugins: [vue()],
-  root: 'src',
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    Vue(),
+
+    // https://github.com/unocss/unocss
+    Unocss({
+      presets: [
+        // https://github.com/unocss/unocss/tree/main/packages/preset-uno
+        presetUno({}),
+        // https://github.com/unocss/unocss/tree/main/packages/preset-icons
+        presetIcons({
+          scale: 1.2,
+        }),
+      ],
+    }),
+  ],
+
   build: {
-    outDir: '../dist',
     emptyOutDir: true,
   },
-}
+
+  // define: {
+  //   __LAST_UPDATE__: JSON.stringify(new Date().toISOString()),
+  // },
+})
