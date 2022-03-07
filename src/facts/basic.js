6c5da75cfbc091ecb88a9705904371f260d57368
Marko Marković <okram@civokram.com>
2022-03-07 19:16:21 +0400

    Some content and style tweaks

--- src/facts/basic.js
+++ src/facts/basic.js
@@ -3,5 +3,5 @@ export default {
   title: `Curriculum Vitae`,
   name: `Marko Marković`,
   profession: `tech lead / software developer`,
-  quote: `Crafts code since 20th century and still thinks it’s fun. Began the professional career with imperative style PHP, transitioned to Object Oriented style for a while but always felt something was not right. Then discovered functional style and got totally corrupted with Node.js. Favorite quote? <i>“I’d rather write programs that write programs than write programs.”</i>`,
+  quote: `Crafts code since 20th century and still thinks it’s fun. Began the professional career with imperative style PHP, transitioned to Object Oriented style for a while but always felt something was not right. Then discovered functional style and got totally corrupted with Node.js. When not coding, plays around with photography, and/or searches for dark places to stargaze from. Favorite quote? <i>“I’d rather write programs that write programs than write programs.”</i>.`,
 }
--- src/facts/quickFacts.js
+++ src/facts/quickFacts.js
@@ -18,7 +18,7 @@ export default [
   },
   {
     icon: 'i-tabler-phone?bg',
-    text: 'Can be reached on <b><abbr title="UAE">+971</abbr> 50 252 6569</b> phone number when in UAE or <b><abbr title="Serbia">+381</abbr> 62 200 512</b> when in Serbia.',
-    // text: 'Can be reached on <b><abbr title="Serbia">+381</abbr> 62 200 512</b> phone number when in Serbia, or <b><abbr title="UAE">+971</abbr> 50 252 6569</b> when in UAE.',
+    text: 'Can be reached on <b><abbr title="UAE">+971</abbr>&nbsp;50&nbsp;252&nbsp;6569</b> phone number when in UAE or <b><abbr title="Serbia">+381</abbr>&nbsp;62&nbsp;200&nbsp;512</b> when in Serbia.',
+    // text: 'Can be reached on <b><abbr title="Serbia">+381</abbr>&nbsp;62&nbsp;200&nbsp;512</b> phone number when in Serbia, or <b><abbr title="UAE">+971</abbr>&nbsp;50&nbsp;252&nbsp;6569</b> when in UAE.',
   },
 ]
